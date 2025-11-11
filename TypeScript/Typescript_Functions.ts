9670533fad3a769b16245caae694747da6430ec5
Tejashvar K R <tejashvarkr@gmail.com>
2025-11-11 14:42:17 +0530

    Explored Call Signature,Constructor Signature,interface extends, implements through class , object and interface implementation

--- TypeScript/Typescript_Functions.ts
+++ TypeScript/Typescript_Functions.ts
@@ -17,7 +17,55 @@ function printers(fns:functions)
     fns("Hello there!");
 }
 //Call Signature and Construct Signature
+type DescribableFunction = {
+  description: string;
+  (someArg: number): boolean;
+};
+function doSomething(fn: DescribableFunction) {
+  console.log(fn.description + " returned " + fn(6));
+}
+ 
+function myFunc(someArg: number) {
+  return someArg > 3;
+}
+myFunc.description = "default description";
+ 
+doSomething(myFunc);
+
+type SomeConstructor = {
+  new (s: string): BooleanClass;
+};
+function fn(ctor: SomeConstructor) {
+  return new ctor("hello");
+}
 
 
+class BooleanClass {
+  constructor(s: string) {
+    return s.length > 4; 
+  }
+}
 
 
+const result = fn(BooleanClass);
+console.log(result);
+
+interface measure
+{
+    length: number,
+
+
+}
+interface Rectangle extends measure {
+ breadth:number;
+ calculate_area(length:number, breadth:number):void
+ 
+}
+let rectangle:Rectangle={
+    length:10,
+    breadth:12,
+  calculate_area(length,breadth){
+    console.log(length*breadth)
+  },
+};
+rectangle.calculate_area(rectangle.length,rectangle.breadth);
