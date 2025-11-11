2f5dd49d9a17a12a4ba42c3e7a428670687cdfc9
Tejashvar K R <tejashvarkr@gmail.com>
2025-11-11 12:44:39 +0530

    typescript prac: 11 nov

--- Typescript_Functions.ts
+++ Typescript_Functions.ts
@@ -11,4 +11,10 @@ printer(print);
 //Understanding how a function is passed as a parameter , and basic structure of function with return value type annotated , and also passing parameter.
 
 //Using type
-type 
+type functions = (b:string)=>void;
+function printers(fns:functions)
+{
+    fns("Hello there!");
+}
+//Call Signature and Construct Signature 
+
