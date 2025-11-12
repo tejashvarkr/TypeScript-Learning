eb402f5c895b679eb267b89f9951e2a1442c8ead
Tejashvar K R <tejashvarkr@gmail.com>
2025-11-12 16:28:52 +0530

    Explored concepts of classes, constructors, inheritance, extends,arrow functions, implements,this parameter,abstract classes

---
+++ TypeScript/Classes.ts
@@ -0,0 +1,88 @@
+import mongoose = require("mongoose");
+
+//Classes
+class Person
+{
+     name:string;
+     readonly username?:string;
+    constructor(name:string, username:string)
+    {
+        this.name=name;
+        this.username=username;
+    }
+    makeSound():void{
+        console.log(`${this.name} is making a sound`)
+    }
+}
+const Person1=new Person('AA','')
+Person1.makeSound();
+
+class shape
+{
+    protected length: number;
+    protected breadth: number;
+
+    constructor(length:number,breadth:number);
+    constructor(length:string,breadth:number);
+    constructor(length:number|string,breadth:number)
+    {
+        this.length = typeof length === 'string' ? Number(length) : length;
+        this.breadth = breadth;
+        console.log("Constructor Overloading occurs when we have two or more constructors with the same name , different numbers");
+    }
+    scale(length: number,breadth:number): void {
+    this.length *= 1;
+    this.breadth *= 1;
+}
+}
+class Triangle extends shape{
+
+    constants:number
+    constructor(length:number,breadth:number,constants:number)
+    {super(length,breadth);
+        this.constants=constants
+    }
+
+      area(): void {
+    console.log(`Area of Triangle = ${(this.length * this.breadth)/this.constants}`);
+  }
+}  
+let triangleobj= new Triangle(1,2,3);
+triangleobj.area();
+console.log(triangleobj.constants);
+console.error(triangleobj.constants);
+console.warn("Hello there")
+
+class C {
+  _length = 0;
+  get length() {
+    return this._length;
+  }
+  set length(value) {
+    this._length = value;
+  }
+}
+class Myclass{
+    [s:string]:boolean | ((s:string)=>boolean);
+    check(s:string)
+    {
+
+    return this[s] as boolean;
+    }
+}
+interface Pingable {
+  ping(): void;
+}
+ 
+class Sonar implements Pingable {
+  ping() {
+    console.log("ping!");
+  }
+}
+ 
+class Ball implements Pingable {
+ping() {
+    console.log("pong!");
+  }
+}
+//Explored extends clause,arrow functions,implements clauses,access modifiers
